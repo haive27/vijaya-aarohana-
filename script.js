ea976e5c26d965034048a3cf3b2e0f91ca2f09b6
haive27 <168862691+haive27@users.noreply.github.com>
2024-07-07 23:14:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,11 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('Document loaded');
     // Add any interactivity you want here
 });
+let counter = 1;
+setInterval(() => {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 4) {
+        counter = 1;
+    }
+}, 5000);
